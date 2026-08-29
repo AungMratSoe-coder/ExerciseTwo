9465caaee7313b9523568cd6b7358c6473bfaf39
AungMratSoe-coder <aungmratsoe@mail.arakkha.net>
2026-08-29 10:56:14 +0630

    this is initial commit

---
+++ src/main/java/exercise/ExerciseTwo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package exercise;
+
+/**
+ *
+ * @author aungmrat
+ */
+public class ExerciseTwo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
