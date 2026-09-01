d0cb7353d7900257505d7f19305ebb74a61c52d7
myonyeinaoo <myonyeinaoo@mail.arakkha.net>
2026-09-01 14:18:19 +0630

    kjhuyoyo

--- src/main/java/exercise/ui/panel/Dev7LoginForm.java
+++ src/main/java/exercise/ui/panel/Dev7LoginForm.java
@@ -64,7 +64,7 @@ public class Dev7LoginForm extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 442, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
